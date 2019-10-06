48d2428e63e8637c7569ce6b23a9815835fac696
Alidu Bakpata Latif <aliduarahim@gmail.com>
2019-10-06 23:54:36 +0000

    Summary expenses header added

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ExpenseList from './expensesList';
 import ExpenseListFilters from './expenseListFilters';
+import ExpensesSummary from './expenses-summary'
 
 
  const Dashboard = () => (
     <div>
+    <ExpensesSummary />
      <ExpenseListFilters />
         <ExpenseList />
-        <p>Dashboard</p>
     </div>
 )
 
--- src/components/expenseListItem.js
+++ src/components/expenseListItem.js
@@ -11,7 +11,7 @@ const ExpenseListItem = ({description, amount, createdAt,id}) => {
   <h3>{description}</h3>
   </Link>
             <p>
-            {numeral(amount / 100).format('$0,0.00')}  
+            {numeral(amount / 100).format('$0,0.00')} 
             - 
             {moment(createdAt).format('MMMM Do, YYYY')}
             </p>
